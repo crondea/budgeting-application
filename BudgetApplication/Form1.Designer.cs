5c8ed1ce574a085085934f67955382cca18106b0
crondea <36548937+crondea@users.noreply.github.com>
2018-05-24 22:34:13 -0400

    Add files via upload

---
+++ BudgetApplication/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace BudgetApplication
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelOverview = new System.Windows.Forms.Label();
+            this.labelIncome = new System.Windows.Forms.Label();
+            this.labelOtherIncomes = new System.Windows.Forms.Label();
+            this.labelIncomes = new System.Windows.Forms.Label();
+            this.radioButtonHousing = new System.Windows.Forms.RadioButton();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.textBoxOtherIncomes = new System.Windows.Forms.TextBox();
+            this.labelExpenses = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonLoan = new System.Windows.Forms.RadioButton();
+            this.radioButtonTransportation = new System.Windows.Forms.RadioButton();
+            this.radioButtonFood = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelOverview
+            // 
+            this.labelOverview.AutoSize = true;
+            this.labelOverview.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOverview.Location = new System.Drawing.Point(347, 26);
+            this.labelOverview.Name = "labelOverview";
+            this.labelOverview.Size = new System.Drawing.Size(246, 32);
+            this.labelOverview.TabIndex = 0;
+            this.labelOverview.Text = "Budget Overview";
+            // 
+            // labelIncome
+            // 
+            this.labelIncome.AutoSize = true;
+            this.labelIncome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIncome.Location = new System.Drawing.Point(55, 283);
+            this.labelIncome.Name = "labelIncome";
+            this.labelIncome.Size = new System.Drawing.Size(206, 22);
+            this.labelIncome.TabIndex = 1;
+            this.labelIncome.Text = "Estimated Yearly Salary:";
+            // 
+            // labelOtherIncomes
+            // 
+            this.labelOtherIncomes.AutoSize = true;
+            this.labelOtherIncomes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOtherIncomes.Location = new System.Drawing.Point(55, 318);
+            this.labelOtherIncomes.Name = "labelOtherIncomes";
+            this.labelOtherIncomes.Size = new System.Drawing.Size(153, 22);
+            this.labelOtherIncomes.TabIndex = 2;
+            this.labelOtherIncomes.Text = "Estimated Others:";
+            // 
+            // labelIncomes
+            // 
+            this.labelIncomes.AutoSize = true;
+            this.labelIncomes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIncomes.Location = new System.Drawing.Point(54, 241);
+            this.labelIncomes.Name = "labelIncomes";
+            this.labelIncomes.Size = new System.Drawing.Size(111, 29);
+            this.labelIncomes.TabIndex = 3;
+            this.labelIncomes.Text = "Incomes";
+            // 
+            // radioButtonHousing
+            // 
+            this.radioButtonHousing.AutoSize = true;
+            this.radioButtonHousing.Location = new System.Drawing.Point(500, 283);
+            this.radioButtonHousing.Name = "radioButtonHousing";
+            this.radioButtonHousing.Size = new System.Drawing.Size(93, 24);
+            this.radioButtonHousing.TabIndex = 4;
+            this.radioButtonHousing.TabStop = true;
+            this.radioButtonHousing.Text = "Housing";
+            this.radioButtonHousing.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(267, 282);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(100, 26);
+            this.textBoxSalary.TabIndex = 5;
+            // 
+            // textBoxOtherIncomes
+            // 
+            this.textBoxOtherIncomes.Location = new System.Drawing.Point(267, 317);
+            this.textBoxOtherIncomes.Name = "textBoxOtherIncomes";
+            this.textBoxOtherIncomes.Size = new System.Drawing.Size(100, 26);
+            this.textBoxOtherIncomes.TabIndex = 6;
+            // 
+            // labelExpenses
+            // 
+            this.labelExpenses.AutoSize = true;
+            this.labelExpenses.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelExpenses.Location = new System.Drawing.Point(495, 241);
+            this.labelExpenses.Name = "labelExpenses";
+            this.labelExpenses.Size = new System.Drawing.Size(127, 29);
+            this.labelExpenses.TabIndex = 7;
+            this.labelExpenses.Text = "Expenses";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(500, 403);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 24);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Housing";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLoan
+            // 
+            this.radioButtonLoan.AutoSize = true;
+            this.radioButtonLoan.Location = new System.Drawing.Point(500, 373);
+            this.radioButtonLoan.Name = "radioButtonLoan";
+            this.radioButtonLoan.Size = new System.Drawing.Size(131, 24);
+            this.radioButtonLoan.TabIndex = 9;
+            this.radioButtonLoan.TabStop = true;
+            this.radioButtonLoan.Text = "Student Loan";
+            this.radioButtonLoan.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonTransportation
+            // 
+            this.radioButtonTransportation.AutoSize = true;
+            this.radioButtonTransportation.Location = new System.Drawing.Point(500, 343);
+            this.radioButtonTransportation.Name = "radioButtonTransportation";
+            this.radioButtonTransportation.Size = new System.Drawing.Size(137, 24);
+            this.radioButtonTransportation.TabIndex = 10;
+            this.radioButtonTransportation.TabStop = true;
+            this.radioButtonTransportation.Text = "Transportation";
+            this.radioButtonTransportation.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonFood
+            // 
+            this.radioButtonFood.AutoSize = true;
+            this.radioButtonFood.Location = new System.Drawing.Point(500, 313);
+            this.radioButtonFood.Name = "radioButtonFood";
+            this.radioButtonFood.Size = new System.Drawing.Size(144, 24);
+            this.radioButtonFood.TabIndex = 11;
+            this.radioButtonFood.TabStop = true;
+            this.radioButtonFood.Text = "Food/Groceries";
+            this.radioButtonFood.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(663, 282);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(663, 312);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(663, 342);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(663, 371);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(663, 402);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 16;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(401, 513);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(87, 42);
+            this.buttonSave.TabIndex = 17;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(280, 513);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(87, 42);
+            this.buttonLoad.TabIndex = 18;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(521, 513);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(87, 42);
+            this.buttonClear.TabIndex = 19;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(881, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Please enter the amounts in dollars ($) for the fields below. Expenses can be inc" +
+    "luded/excluded based upon personal needs.";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 600);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButtonFood);
+            this.Controls.Add(this.radioButtonTransportation);
+            this.Controls.Add(this.radioButtonLoan);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.labelExpenses);
+            this.Controls.Add(this.textBoxOtherIncomes);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.radioButtonHousing);
+            this.Controls.Add(this.labelIncomes);
+            this.Controls.Add(this.labelOtherIncomes);
+            this.Controls.Add(this.labelIncome);
+            this.Controls.Add(this.labelOverview);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelOverview;
+        private System.Windows.Forms.Label labelIncome;
+        private System.Windows.Forms.Label labelOtherIncomes;
+        private System.Windows.Forms.Label labelIncomes;
+        private System.Windows.Forms.RadioButton radioButtonHousing;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.TextBox textBoxOtherIncomes;
+        private System.Windows.Forms.Label labelExpenses;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButtonLoan;
+        private System.Windows.Forms.RadioButton radioButtonTransportation;
+        private System.Windows.Forms.RadioButton radioButtonFood;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
